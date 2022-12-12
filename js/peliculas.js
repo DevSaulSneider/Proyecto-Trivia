27d9ed70a0dea4dfd61fc2ddb8c59f7e48ba4d13
DevSaulSneider <saulsneiderwork@outlook.com>
2022-12-12 11:01:09 -0500

    Avance de funciones

--- js/peliculas.js
+++ js/peliculas.js
@@ -15,5 +15,37 @@ const preguntas =[
         respuesta: ["Jack Dawson y Rose Dewitt Bukater","Noa Caulhon y Allie Hamilton","Ryan Reynolds y Blake Lively", "Romeo Montesco y Julieta Capuleto"],
         correcta: 0
     },
+];
 
-]
+var indice_aleatorio = 0;
+var pregunta_txt = "";
+var interval;
+
+window.onload = inciar();
+
+function inciar(){
+    if(localStorage.getItem("SCORE") != null){
+        localStorage.removeItem("SCORE");
+    }
+}
+
+function inciarCronometro(){
+    const contador = 15, cronometroDisplay = document.getElementById("cronometro")
+
+    inciarTiempo(contador, cronometroDisplay)
+}
+
+function inciarTiempo(duracion, componente){
+    interval = setInterval(() => {
+        if (duracion == 0) {
+            componente.innerHTML="Se acabo el tiempo";
+
+            clearInterval(interval);
+
+            loadQuestions()
+        }else{
+            duracion = duracion<10 ? "0" + duracion:duracion--;
+        }
+
+    }, 1000);
+}
