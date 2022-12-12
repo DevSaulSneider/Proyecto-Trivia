23d491cb6915a467496d28c3dec6a2c942b49912
DevSaulSneider <saulsneiderwork@outlook.com>
2022-12-12 10:20:16 -0500

    first commit

---
+++ js/peliculas.js
@@ -0,0 +1,19 @@
+const preguntas =[
+    {
+        pregunta: "¿En que pelicula aparece Jack Nicholson?",
+        respuesta: ["50 sombras de Geyt","El resplandor","Intensamente", "Silen Hill"],
+        correcta: 1
+    },
+    
+    {
+        pregunta: "¿En que genero de cine se encuentra la pelicula V de vendetta?",
+        respuesta: ["Triller psicologico","Drama","Ciencia Ficcion", "Romance"],
+        correcta: 2
+    },
+    {
+        pregunta: "¿Cuales eran los personajes principales del TITANIC?",
+        respuesta: ["Jack Dawson y Rose Dewitt Bukater","Noa Caulhon y Allie Hamilton","Ryan Reynolds y Blake Lively", "Romeo Montesco y Julieta Capuleto"],
+        correcta: 0
+    },
+
+]
